6330b03674bcd3ec9eba5176ca2d75c40944f9d0
witek <witekdev@gmail.com>
2013-01-15 14:56:54 +0200

    add unit testing for pagetrackingviewlet.

--- upfront/pagetracker/tests/base.py
+++ upfront/pagetracker/tests/base.py
@@ -1,8 +1,11 @@
 from zope.app.intid.interfaces import IIntIds
 from zope.component import getUtility
-from zope.event import notify
-from zope.lifecycleevent import ObjectModifiedEvent
-from z3c.relationfield import RelationValue
+
+from zope.interface import alsoProvides 
+from zope.component import queryMultiAdapter
+from zope.viewlet.interfaces import IViewletManager
+
+from Products.Five.browser import BrowserView as View
 
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import PLONE_FIXTURE
@@ -44,5 +47,21 @@ class UpfrontPageTrackerTestBase(unittest.TestCase):
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         self.intids = getUtility(IIntIds)
         self.request = self.layer['request']
+
+    def _find_viewlet(self, context, manager_name, viewlet_name, layer=None):
+        request = self.portal.REQUEST
+        if layer:
+            alsoProvides(request, layer)
+
+        view = View(context, request)
+        manager = queryMultiAdapter((context, request, view),
+                  IViewletManager, manager_name, default=None)
+        self.failUnless(manager)
+
+        manager.update()
+        viewlets = manager.viewlets
+        viewlet = [v for v in viewlets if v.__name__ == viewlet_name]
+        return viewlet
+
 
 
---
+++ upfront/pagetracker/tests/test_viewlets.py
@@ -0,0 +1,33 @@
+import datetime
+from zope.component import getUtility
+
+from upfront.pagetracker.interfaces import IUpfrontPageTrackerLayer
+from upfront.pagetracker.interfaces import IPageTracker
+
+from base import UpfrontPageTrackerTestBase
+
+class TestPageTrackingViewlet(UpfrontPageTrackerTestBase):
+    """ Basic methods to test page tracking viewlet """
+
+    def test_logrequest(self):
+
+        context = self.portal
+        manager_name = 'plone.portalfooter'
+        viewlet_name = 'pagetrackingviewlet'
+        layer = IUpfrontPageTrackerLayer
+        viewlet = self._find_viewlet(context, manager_name, viewlet_name, layer)
+
+        viewlet[0].update()
+        now = datetime.datetime.now()
+        datetime_str = now.strftime('%d/%m/%Y %H:%M:%S')
+
+        pagetracker = getUtility(IPageTracker)
+        result = pagetracker.logged_data()
+        self.assertEqual(result[0][1][0]['url'],'http://nohost')
+        self.assertEqual(result[0][1][0]['user'],'test-user')
+
+        # final assertion test is commented out as it depends on exact time,
+        # it works but perhaps it could fail someday due to something taking
+        # longer
+        # self.assertEqual(result[0][1][0]['time'],datetime_str)
+
